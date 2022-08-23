96959ab0a2782e6020ee0cc9cc7827430704eaa3
Fabowale Awwal <fabowalemuhawwal@gmail.com>
2022-08-23 21:32:28 +0100

    mmmmm

--- src/components/BuildFuture.vue
+++ src/components/BuildFuture.vue
@@ -19,7 +19,7 @@ export default {
         <div class="btitle mt-[90px] h-[400px] md:h-[400px] text-white md:w-full rounded-2xl bg-blue-700 py-[30px] px-[25px] md:py-[50px] md:px-[40px] lg:px-[60px]">
             <div class="md:w-[60%] lg:w-[30%] mt-[30px]">
                 <div>
-                    <img src="img/collect.svg" class="" alt=""/>
+                    <img src="/collect.svg" class="" alt=""/>
                     <div class="font-bold mt-[20px] text-[25px] md:text-[40px]">Connect</div>
                     <div class="mt-[15px] font-bold text-[16px] md:text-[16px]">Securely access financial accounts for statements, transactions, and identity</div>
                     <div class="btitle mt-[30px]">
@@ -31,19 +31,19 @@ export default {
         <!--moving to the next two build futures-->
         <div class="btitle md:flex md:justify-between mt-[30px]">
             <div class="md:w-[48%] h-[500px] md:h-[650px] w-full rounded-2xl py-[30px] px-[25px] md:py-[30px] bg-gray-200 md:px-[40px]">
-                <div><img src="dpay.svg" alt=""/></div>
+                <div><img src="/dpay.svg" alt=""/></div>
                 <div class="mt-[20px] md:mt-[20px] text-black text-[25px] md:text-[40px] font-bold">DirectPay</div>
                 <div class="mt-[20px] md:mt-[20px] font-bold text-gray-900 md:w-[80%] text-[18px]">Collect bank payments in your web or mobile app. No cards. No chargebacks</div>
                 <div class="mt-[20px] md:mt-[20px]">
                     <button class="bg-blue-800 text-white rounded-xl font-bold px-[20px] py-[10px]">Start with DirectPay <i class="las la-arrow-right"></i></button>
                 </div>
                 <div class="mt-[20px] flex w-fit h-fit bg-gray-50 py-[10px] px-[20px] rounded-full">
-                    In Partnership with <img src="flutter-logo.svg" class="w-[100px] ml-[10px]" alt=""/>
+                    In Partnership with <img src="/flutter-logo.svg" class="w-[100px] ml-[10px]" alt=""/>
                 </div>
             </div>
             <!--second phase-->
             <div class="mt-[30px] md:mt-[0px] md:w-[48%] h-[500px] md:h-[650px] w-full rounded-2xl py-[30px] px-[25px] md:py-[30px] bg-black md:px-[40px]">
-                <div class="w-[35px]"><img src="per.svg" class="w-full" alt=""/></div>
+                <div class="w-[35px]"><img src="/per.svg" class="w-full" alt=""/></div>
                 <div class="mt-[20px] md:mt-[20px] text-white text-[25px] md:text-[40px] font-bold">Percept</div>
                 <div class="mt-[20px] md:mt-[20px] font-bold text-gray-100 md:w-[80%] text-[18px]">Money operations and reconciliation for all your corporate accounts, in one dashboard</div>
                 <div class="mt-[20px] md:mt-[20px]">
